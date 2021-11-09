04f72ac27d11a7b16fb0904540f81f3c8ffbbb10
preet665 <47426321+preet665@users.noreply.github.com>
2021-11-09 13:44:42 +0530

    commit

---
+++ showledger.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Windows.Forms.VisualStyles;
+using System.Diagnostics;
+using Spire;
+
+namespace thalbhet
+{
+    public partial class showledger : Form
+    {
+        SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString);
+        public showledger(string smkid)
+        {
+            InitializeComponent();
+            label1.Text = smkid;
+        }
+
+        private void showledger_Load(object sender, EventArgs e)
+        {
+            SqlCommand selectCMD = new SqlCommand("SELECT * FROM newentrytable where SMK LIKE '" + label1.Text + "'", con);
+            SqlDataAdapter DA = new SqlDataAdapter();
+            DA.SelectCommand = selectCMD;
+            con.Open();
+            DataSet DS = new DataSet();
+            DA.Fill(DS, "newentrytable");
+            dataGridView1.DataSource = DS.Tables["newentrytable"].DefaultView;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.SelectAll();
+            DataObject dataObj = dataGridView1.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+            Microsoft.Office.Interop.Excel.Application xlexcel;
+            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+            xlexcel = new Microsoft.Office.Interop.Excel.Application();
+            xlexcel.Visible = true;
+            xlWorkBook = xlexcel.Workbooks.Add(misValue);
+            xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            Microsoft.Office.Interop.Excel.Range CR = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[1, 1];
+            CR.Select();
+            xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PdfPTable pdfTable = new PdfPTable(dataGridView1.ColumnCount);
+            pdfTable.DefaultCell.Padding = 5;
+            pdfTable.WidthPercentage = 100;
+            pdfTable.HorizontalAlignment = Element.ALIGN_JUSTIFIED;
+            pdfTable.DefaultCell.BorderWidth = 2;
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                cell.BackgroundColor = new iTextSharp.text.BaseColor(30, 144, 255);
+                pdfTable.AddCell(cell);
+            }
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    if (cell.Value == null)
+                    {
+
+                    }
+                    else
+                    {
+                        pdfTable.AddCell(cell.Value.ToString());
+                    }
+                }
+            }
+            string folderpath = "E:\\PDF\\";
+            if (!Directory.Exists(folderpath))
+            {
+                Directory.CreateDirectory(folderpath);
+            }
+            using (FileStream stream = new FileStream(folderpath + "LedgerReport.pdf", FileMode.Create))
+            {
+                Document document = new Document(PageSize.A4.Rotate(), 10f, 10f, 10f, 0f);
+                PdfWriter.GetInstance(document, stream);
+                document.Open();
+                document.Add(pdfTable);
+                document.Close();
+                stream.Close();
+                MessageBox.Show("Pdf is exported to E:\\LedgerReport.pdf");
+            }
+        }
+    }
+}
